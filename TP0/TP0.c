e3fffedb8499813712c5cf3dfebeb534445f4fbc
RemersaroRamiro <167626573+RemersaroRamiro@users.noreply.github.com>
2024-04-20 17:23:27 -0300

    Add files via upload

---
+++ TP0/TP0.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+    // Ejercicio 3 //
+    printf("Ejercicio 3:\n");
+
+    printf("Hola Mundo\n");
+    // Final de ejercicio 3 //
+    // Ejercicio 4 //
+    printf("Ejercicio 4:\n");
+    
+    char nombre[50];
+    printf("Ingrese nombre completo: ");
+    scanf("%s", nombre);
+    printf("\nHola %s ", nombre);
+    // Final de ejercicio 4 //
+    return 0;
+}
